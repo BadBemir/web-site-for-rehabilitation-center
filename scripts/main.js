0b5d2ae48d040e10993b53b01368addde2b0249d
PavliK <badbemir@gmail.com>
2026-02-13 18:09:28 +1000

    add preload animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,20 @@
 "use strict"
 
+
+  window.addEventListener('load', function() {
+    const preloader = document.getElementById('preloader');
+    preloader.style.opacity = '0';
+    setTimeout(() => {
+      preloader.style.display = 'none';
+    }, 600); // время совпадает с transition в CSS
+  });
+
+
 const burger = document.getElementById('burgerMenu');
 const nav = document.getElementById('navLinks');
 
 burger.addEventListener('click', () => { 
     nav.classList.toggle('active');
 });
+
+
